122babb5f3090679af29966578cd65b4187f8057
Bezlpetr <33637105+Bezlpetr@users.noreply.github.com>
2021-07-14 13:51:21 +0300

    Исправляет замечания по результатам проверки проекта

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,8 @@
+const wrapper = document.querySelector(".modal-wrapper");
 const popup = document.querySelector(".modal-sending");
 const button_wtite = document.querySelector(".button-write-to-us");
 const button_write_close = document.querySelector(".modal-close");
-const name = popup.querySelector("[name=sender-name]");
+const popupName = popup.querySelector("[name=sender-name]");
 const form = popup.querySelector(".sending-form");
 const email = popup.querySelector("[name=sender-email]");
 const text = popup.querySelector("[name=sender-text]");
@@ -21,9 +22,10 @@ try {
 }
 button_wtite.addEventListener("click", function (evt) {
   evt.preventDefault();
+  wrapper.classList.add("wrapper-visible");
   popup.classList.add("modal-visible");
   if (storage_name != null && storage_email != null) {
-    name.value = storage_name;
+    popupName.value = storage_name;
     email.value = storage_email;
     text.focus();
   }
@@ -32,18 +34,19 @@ button_wtite.addEventListener("click", function (evt) {
 button_write_close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-visible");
+  wrapper.classList.remove("wrapper-visible");
   popup.classList.remove("modal-error");
 });
 
 form.addEventListener("submit", function (evt) {
-  if (!name.value || !email.value || !text.value) {
+  if (!popupName.value || !email.value || !text.value) {
     evt.preventDefault();
     popup.classList.add("modal-error");
     popup.offsetWidth = popup.offsetWidth;
     popup.classList.add("modal-error");
   } else {
     if (isStorageSupport) {
-      localStorage.setItem("name", name.value);
+      localStorage.setItem("name", popupName.value);
       localStorage.setItem("email", email.value);
     }
   }
@@ -54,6 +57,7 @@ window.addEventListener("keydown", function (evt) {
     if (popup.classList.contains("modal-visible")) {
       evt.preventDefault();
       popup.classList.remove("modal-visible");
+      popup.classList.remove("wrapper-visible");
       popup.classList.remove("modal-error");
     }
   }
